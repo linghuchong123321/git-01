919932c6be08a79c349308bdf75c269316449953
dashi <1165371095@qq.com>
2019-11-05 16:19:47 +0800

    第一次提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: wb_cheng
+ * @Date: 2019/11/5 16:18
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("第一次提交github");
+    }
+}
